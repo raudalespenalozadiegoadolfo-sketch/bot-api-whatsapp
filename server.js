3062f1b56e02da909577632ed612b9d7b918614f
Diego <raudalespenalozadiegoadolfo@gmail.com>
2026-07-12 09:51:57 -0600

    Usar menuService como menu unico

--- server.js
+++ server.js
@@ -9,9 +9,8 @@ const crypto = require("crypto");
 const webhookRoutes = require("./routes/webhookRoutes");
 const Cliente = require("./models/Cliente");
 const ProcessedMessage = require("./models/ProcessedMessage");
-const {
-  detectProducts,
-} = require("./services/productMatcherService");
+const { products } = require("./services/menuService");
+const { detectProducts } = require("./services/productMatcherService");
 
 const app = express();
 
@@ -48,58 +47,6 @@ for (const [name, value] of Object.entries(REQUIRED_ENV)) {
 /* =========================
    MENÚ
 ========================= */
-
-const products = [
-  ["food", "Camarones", "Camarones a la diabla", 180, ["diabla", "camarones diabla"]],
-  ["food", "Camarones", "Camarones empanizados", 190, ["camaron empanizado", "camarones empanizados"]],
-  ["food", "Camarones", "Camarones al ajo", 180, ["ajo", "camarones ajo"]],
-  ["food", "Camarones", "Camarones al ajillo", 180, ["ajillo", "camarones ajillo"]],
-  ["food", "Pulpo", "Pulpo a la diabla", 220, ["pulpo diabla"]],
-  ["food", "Pulpo", "Pulpo empanizado", 220, ["pulpo empanizado"]],
-  ["food", "Pulpo", "Pulpo zarandeado", 220, ["pulpo zarandeado"]],
-  ["food", "Filete", "Filete a la diabla", 160, ["filete diabla"]],
-  ["food", "Filete", "Filete empanizado", 170, ["filete empanizado"]],
-  ["food", "Filete", "Filete al ajo", 170, ["filete ajo"]],
-  ["food", "Cocteles", "Coctel de camarón", 190, ["coctel camaron", "cóctel de camarón"]],
-  ["food", "Cocteles", "Coctel de pulpo", 200, ["coctel pulpo"]],
-  ["food", "Cocteles", "Coctel de callo", 250, ["coctel callo"]],
-  ["food", "Cocteles", "Coctel mixto", 220, ["coctel mixto"]],
-  ["food", "Ceviches", "Ceviche de pescado", 180, ["ceviche pescado"]],
-  ["food", "Ceviches", "Ceviche de camarón", 200, ["ceviche camaron"]],
-  ["food", "Aguachiles", "Aguachile verde", 190, ["aguachile verde"]],
-  ["food", "Aguachiles", "Aguachile rojo", 190, ["aguachile rojo"]],
-  ["food", "Aguachiles", "Aguachile negro", 190, ["aguachile negro"]],
-  ["food", "Cortes", "Arrachera", 220, ["arrachera"]],
-  ["food", "Cortes", "T-bone", 250, ["t bone", "tbone"]],
-  ["food", "Cortes", "Rib eye", 270, ["ribeye", "rib eye"]],
-  ["drink", "Refrescos", "Coca Cola", 30, ["coca", "coca cola"]],
-  ["drink", "Refrescos", "Coca Cola Light", 30, ["coca light", "coca cola light"]],
-  ["drink", "Refrescos", "Pepsi", 25, ["pepsi"]],
-  ["drink", "Refrescos", "Sangría", 25, ["sangria"]],
-  ["drink", "Refrescos", "7Up", 25, ["7up", "seven"]],
-  ["drink", "Aguas", "Agua de jamaica", 35, ["jamaica", "agua jamaica"]],
-  ["drink", "Aguas", "Agua de arroz", 35, ["arroz", "agua de arroz", "horchata"]],
-  ["drink", "Aguas", "Agua de piña", 35, ["piña", "pina", "agua piña"]],
-  ["drink", "Aguas", "Agua de limón", 35, ["limon", "limón", "agua limon"]],
-  ["drink", "Micheladas", "Michelada de camarón", 100, ["michelada camaron"]],
-  ["drink", "Micheladas", "Michelada Clamato", 80, ["michelada clamato", "clamato"]],
-  ["drink", "Micheladas", "Michelada tamarindo", 90, ["michelada tamarindo"]],
-  ["drink", "Cervezas", "Corona Extra", 40, ["corona", "corona extra"]],
-  ["drink", "Cervezas", "Corona Light", 40, ["corona light"]],
-  ["drink", "Cervezas", "Corona Cero", 40, ["corona cero"]],
-  ["drink", "Cervezas", "Tecate", 35, ["tecate"]],
-  ["drink", "Cervezas", "Tecate Light", 35, ["tecate light"]],
-  ["drink", "Cervezas", "Indio", 30, ["indio"]],
-  ["drink", "Cervezas", "Ultra", 30, ["ultra"]],
-  ["drink", "Cervezas", "Heineken Cero", 35, ["heineken cero"]],
-].map(([type, category, name, price, aliases], index) => ({
-  id: `p${index}`,
-  type,
-  category,
-  name,
-  price,
-  aliases
-}));
 
 /* =========================
    MODELOS
